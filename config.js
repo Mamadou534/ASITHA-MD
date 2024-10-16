307d7aeb37912f1a70ecfd92586032c160cd45ca
Mamadou534 <mmndy2002@gmail.com>
2024-10-16 18:07:48 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=zzgkXJyJ#4Y6_ecPwK1V0fc5brKGhG9x9R12UZ8t3R2AxUcYh-gQ",
+MONGODB: process.env.MONGODB || "mongodb+srv://Aizen:Mmndy@cluster0.bnbgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",//enter mongo db url
 };
